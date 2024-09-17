83d5c9a80b802c59544222f64fedd7d2a92b5950
Matheus Vinicius <moraismvgm@gmail.com>
2024-09-17 20:46:40 -0300

    salvo1

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal
  {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da conta:");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o numero da agencia:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome:");
+        String cliente = scanner.next();
+
+        System.out.println("Digite o numero da conta:");
+        Double saldo = scanner.nextDouble();
+
+        System.out.println("Olá" + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é" + agencia + "," + conta + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
